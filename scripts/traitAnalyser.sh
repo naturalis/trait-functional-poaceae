fef7a44e8c8b71295f230f7f6e04057d0f8dd44e
T v D <T.vanDuffelen@student.han.nl>
2021-04-22 19:42:58 +0200

    Updated query to get traits, total, and unique (1 per species) occurance

--- scripts/traitAnalyser.sh
+++ scripts/traitAnalyser.sh
@@ -2,13 +2,16 @@
 #Poaceae page ID: 8223
 
 PAGEID=4075
-QUERY='MATCH (children:Page)-[:parent*]->(ancestor:Page {page_id: '$PAGEID'}),
-       (children)-[:trait]->(trait:Trait)-[:predicate]->(pred:Term)
-       //WHERE children.canonical STARTS WITH "Triticum aestivum"
-       RETURN children.canonical, pred.name
+QUERY='MATCH (child:Page)-[:parent*]->(ancestor:Page {page_id: '$PAGEID'}),
+       (child)-[:trait]->(trait:Trait)-[:predicate]->(pred:Term)
+       //WHERE child.canonical STARTS WITH "Triticum aestivum"
+
+       WITH child, pred, count(pred.name) AS traits, 1 AS species
+       RETURN pred.name AS trait, SUM(traits) AS total_occurance, SUM(species) AS unique_occurance
+       ORDER BY total_occurance DESC
        //LIMIT 100;'
 
 curl https://eol.org/service/cypher \
   -H "Authorization: JWT $(cat eol_token.txt)" \
-  -d "query=$QUERY"
+  -d "query=$QUERY&format=csv"
 
