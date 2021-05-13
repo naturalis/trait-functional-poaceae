3f3612c2438a43f69296656ea48688037165a514
T v D <T.vanDuffelen@student.han.nl>
2021-05-13 14:51:35 +0200

    Added comments and renamed returned columns

--- scripts/traitGetData.sh
+++ scripts/traitGetData.sh
@@ -8,13 +8,20 @@ TRAIT='http://top-thesaurus.org/annotationInfo?viz=1\u0026\u0026trait=Seed_dry_m
 EOLTOKEN=$(cat eol.token)
 
 
+#Query to fetch all children of an ancestor with specified page_id, and collect
+#all measurements they have for the trait Seed Dry Mass.
 
+#This query returns four columns. The first column contains the scientific name
+#for an organism. The second column stores the name of the trait collected.
+#The third column has the trait measurement made for that species.
+#The fourth column contains the unit the measurement is made in.
 QUERY="MATCH (child:Page)-[:parent*]->(ancestor:Page {page_id: $PAGEID}),
        (child)-[:trait]->(trait:Trait)-[:predicate]->(pred:Term {uri: \"$TRAIT\"})
        //WHERE child.canonical STARTS WITH 'Triticum aestivum'
        OPTIONAL MATCH (trait)-[:units_term]->(unit:Term)
 
-       RETURN child.canonical, pred.name, trait.measurement, unit.name
+       RETURN child.canonical AS organism, pred.name AS trait,
+              trait.measurement AS measurement, unit.name AS unit
        //LIMIT 100"
 
 #Use cURL to get the data from the EoL database in CSV format.
